766150982189de8d8c16bdc4dc125dbe8a2895d4
emilysoft <aura.git@proton.me>
2026-08-23 12:10:53 -0400

    fix(antiwalltexts): change parentid to channel.id

--- src/functions/automod/spam/antiTextWall.ts
+++ src/functions/automod/spam/antiTextWall.ts
@@ -59,7 +59,7 @@ export default {
       if (message.channel.parentId === "1169624626188521563") return; // registro principales
       if (message.channel.parentId === "1120080747668197436") return; // registro secundarios
       if (message.channel.parentId === "874730574089187359") return; //extralaborales
-      if (message.channel.parentId == "1416531887903740045") return; // gaming-y-anime
+      if (message.channel.id == "1416531887903740045") return; // gaming-y-anime
       if (message.channel.id === "1005354020333948988") return; //basados
       if (message.author.id == "1095572785482444860") return; // hiraku
 
